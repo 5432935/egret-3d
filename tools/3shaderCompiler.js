2518f3023282c50ecb03e66e895a20c60bc72d57
mebiusashan <mcewei@qq.com>
2017-03-13 11:45:01 +0800

    添加一个新工具

---
+++ tools/3shaderCompiler.js
@@ -0,0 +1,42 @@
+var fs = require("fs");
+
+function parseShader(file, path) {
+    var buffer = fs.readFileSync(path + file);
+    var string = buffer.toString().replace(/[ \t]*\/\/.*\n/g, '')
+          .replace(/[ \t]*\/\*[\s\S]*?\*\//g, '')
+          .replace(/\n{2,}/g, '\n');
+
+    return string;
+}
+
+function compileShader(inputPath, outputPath, outputName) {
+    fs.readdir(inputPath, function(err, files) {
+        if(err) {
+            return console.log(err);
+        }
+
+        var all = "module egret3d {/*** @private*/export class ShaderStore {static lib: { [key:string]: string } = {\n";
+
+        for(var i = 0; i < files.length; i++) {
+            var file = files[i];
+
+            if(file.indexOf(".glsl") > 0) {
+                var name = file.replace(/\.glsl$/, "");
+
+                all += name + ": " + JSON.stringify(parseShader(file, inputPath)) + ",\n";
+            }
+        }
+
+        all += "};}}\n";
+
+        fs.writeFile(outputPath + outputName +".ts", all, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file " + outputName + ".ts" + " was saved!");
+        });
+    });
+}
+
+// exports.compileShader = compileShader;
+compileShader("../shader/", "../engine/src/shader/", "ShaderStore");
