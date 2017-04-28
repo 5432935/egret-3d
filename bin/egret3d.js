63bdcbcded7aaa7a648624725877e14eb7570894
ShawnXie <shawn0326@163.com>
2017-04-28 18:23:05 +0800

    fix bug

--- bin/egret3d.js
+++ bin/egret3d.js
@@ -14821,7 +14821,9 @@ var egret3d;
                 this.gpuSkeletonPose.initSkeletonPose(this._animState[name].clip.getSkeletonPose(0), this._lerpBPose);
             }
             // 防止第一帧显示错误
-            this.update(0, 0);
+            if (!a) {
+                this.update(0, 0);
+            }
         };
         //先算出交换动作后的时间起始点
         //获取融合时间轴上的 指定时间的 a clip
